a625a7a552fae4959b5bbaf9553d2b97fcf8539e
William Ibarra <ibarraw@sheridancollege.ca>
2023-04-05 10:52:15 -0400

    Added UnwindToHome

--- Curo/ViewController.swift
+++ Curo/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func unwindToHomeViewController(segue: UIStoryboardSegue)
+     {
+         
+     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
